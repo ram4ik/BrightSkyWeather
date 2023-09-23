0d454bafcb4d2c05a6d54082cfc7972f1ae1077b
Ramill <ram4ik@hotmail.com>
2023-09-23 16:18:54 +0300

    Location Manager

--- BrightSkyWeather/Controllers/WeatherViewController.swift
+++ BrightSkyWeather/Controllers/WeatherViewController.swift
@@ -8,13 +8,32 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+    
+    private let primaryView = CurrentWeatherView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemBackground
+        setUpView()
+        getLocation()
+    }
+    
+    private func getLocation() {
+        LocationManager.shared.getCurrentLocation { location in
+            print(String(describing: location))
+        }
     }
 
+    private func setUpView() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(primaryView)
+        NSLayoutConstraint.activate([
+            primaryView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            primaryView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            primaryView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            primaryView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
 
 }
 
--- BrightSkyWeather/Views/CurrentWeatherView.swift
+++ BrightSkyWeather/Views/CurrentWeatherView.swift
@@ -7,14 +7,15 @@
 
 import UIKit
 
-class CurrentWeatherView: UIView {
+final class CurrentWeatherView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .green
+        translatesAutoresizingMaskIntoConstraints = false
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
 }
